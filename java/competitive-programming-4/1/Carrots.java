7d2439b6fd824d1d9683a28b2b2a546813696f6e
John Patrick Amata <johnamata97@gmail.com>
2021-01-25 16:12:23 +0800

    solve

---
+++ java/competitive-programming-4/1/Carrots.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Carrots{
+
+    public static void main(String[]args){
+	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int p = sc.nextInt();
+	System.out.println(p);
+    }
+}
--- java/competitive-programming-4/1/QALY.java
+++ java/competitive-programming-4/1/QALY.java
@@ -1,6 +1,17 @@
+import java.util.*;
 public class QALY{
     public static void main(String[]args){
 	Scanner sc = new Scanner(System.in);
-	
+	int n = sc.nextInt();
+	int i = 0;
+	double qaly = 0;
+	while (i < n){
+	    double quality = sc.nextDouble();
+	    double years = sc.nextDouble();
+	    qaly += quality * years;
+	    i++;
+	}
+	System.out.println(qaly);
     }
 }
+
