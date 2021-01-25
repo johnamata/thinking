96b60ad441a0812d9100843cbd8d61aae1703363
John Patrick Amata <johnamata97@gmail.com>
2021-01-25 17:37:33 +0800

    solve

---
+++ java/competitive-programming-4/1/Faktor.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Faktor{
+    public static void main(String[]args){
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt(); int i = sc.nextInt();
+	System.out.println((i-1) * a + 1);
+    }
+}
