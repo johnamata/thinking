bf54eb6184b4221ac5f6b896470041394422c31d
John Patrick Amata <johnamata97@gmail.com>
2021-01-29 02:10:23 +0800

    cp4 book grinding

---
+++ java/competitive-programming-4/1/1.4a/TheLastProblem.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class TheLastProblem{
+    public static void main(String[]args){
+	Scanner sc = new Scanner(System.in);
+	//.strip() to get out the spaces if there's any
+	String s = sc.nextLine().strip();
+	System.out.println("Thank you, " + s +", and farewell!");
+    }
+}
