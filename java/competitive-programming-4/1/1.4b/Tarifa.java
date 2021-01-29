b42c2663e5a63d3efdc5e907ec122947e7dfddc2
John Patrick Amata <johnamata97@gmail.com>
2021-01-29 13:49:11 +0800

    cp4 grind

--- java/competitive-programming-4/1/1.4b/Tarifa.java
+++ java/competitive-programming-4/1/1.4b/Tarifa.java
@@ -3,6 +3,12 @@ import java.util.*;
 public class Tarifa{
     public static void main(String[]args){
 	Scanner sc = new Scanner(System.in);
-	
+	int x = sc.nextInt();
+	int n = sc.nextInt();
+	int sum = 0;
+	for(int i = 0; i < n; i++){
+	    sum += sc.nextInt();
+	}
+	System.out.println(10 + ((n *10) - sum));	
     }
 }
