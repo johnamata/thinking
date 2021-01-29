bddbbdbf1f803638fe4f278d7c5c3a3b74b45066
John Patrick Amata <johnamata97@gmail.com>
2021-01-29 14:07:59 +0800

    cp4 grind

--- java/competitive-programming-4/1/1.4b/TimeLoop.java
+++ java/competitive-programming-4/1/1.4b/TimeLoop.java
@@ -3,6 +3,12 @@ import java.util.*;
 public class TimeLoop{
     public static void main(String[]args){
 	Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int i = 1;
+	while(i <= n){
+	    System.out.println(i + " Abracadabra");
+	    i++;
+	}
 	
     }
 }
