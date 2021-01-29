88d962dca7921331a92bb1b8b0eb5e7fdea8d6b5
John Patrick Amata <johnamata97@gmail.com>
2021-01-29 16:32:41 +0800

    cp4 grind

--- java/competitive-programming-4/1/1.4c/OneChicken.java
+++ java/competitive-programming-4/1/1.4c/OneChicken.java
@@ -1,8 +1,21 @@
 import java.util.*;
 
-public class {
+public class OneChicken{
     public static void main(String[]args){
 	Scanner sc = new Scanner(System.in);
-	
+	int N = sc.nextInt();
+	int M = sc.nextInt();
+	int diff = M-N;
+	int P, Q, chicken;
+	if(diff == 1){
+	    System.out.println("Dr. Chaz will have " + Math.abs(diff) + " piece of chicken left over!");
+	} else if (diff > 0) {
+	    System.out.println("Dr. Chaz will have " + Math.abs(diff) + " pieces of chicken left over!");
+
+	} else if (diff == -1){
+	    System.out.println("Dr. Chaz needs " + Math.abs(diff) + " more piece of chicken!");	    
+	} else {
+	    System.out.println("Dr. Chaz needs " + Math.abs(diff) + " more pieces of chicken!");
+	}
     }
 }
