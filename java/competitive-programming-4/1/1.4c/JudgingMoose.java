82be9878439033c9aa8c69492ff686b49001ba95
John Patrick Amata <johnamata97@gmail.com>
2021-01-29 14:32:17 +0800

    cp4 grind

--- java/competitive-programming-4/1/1.4c/JudgingMoose.java
+++ java/competitive-programming-4/1/1.4c/JudgingMoose.java
@@ -1,8 +1,18 @@
 import java.util.*;
 
-public class {
+public class JudgingMoose{
     public static void main(String[]args){
 	Scanner sc = new Scanner(System.in);
+	int l = sc.nextInt();
+	int r = sc.nextInt();
+
+	if (l+r == 0) {
+	    System.out.println("Not a moose");
+	} else if(l == r){
+	    System.out.println("Even " + Math.max(l,r) * 2);	    
+	} else {
+	    System.out.println("Odd " + Math.max(l,r) * 2);
+	}
 	
     }
 }
