4ffbd788a96395f6a262c97070b7b7f25a10fe2e
John Patrick Amata <johnamata97@gmail.com>
2021-01-29 14:59:33 +0800

    cp4 grind

--- java/competitive-programming-4/1/1.4c/MoscowDream.java
+++ java/competitive-programming-4/1/1.4c/MoscowDream.java
@@ -1,8 +1,13 @@
 import java.util.*;
 
-public class {
+public class MoscowDream{
     public static void main(String[]args){
 	Scanner sc = new Scanner(System.in);
-	
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	int c = sc.nextInt();
+	int total = a+b+c;
+	int n = sc.nextInt();
+	System.out.println(((a >= 1) && (b >= 1) && (c >= 1) && (total >= n) && (n >= 3)) ? "YES" : "NO");   
     }
 }
