6f45505d8cf5cfd18ff101142ae01bc293577d12
John Patrick Amata <johnamata97@gmail.com>
2021-01-29 14:18:41 +0800

    cp4 grind

--- java/competitive-programming-4/1/1.4b/IsItHalloween.java
+++ java/competitive-programming-4/1/1.4b/IsItHalloween.java
@@ -3,6 +3,12 @@ import java.util.*;
 public class IsItHalloween{
     public static void main(String[]args){
 	Scanner sc = new Scanner(System.in);
+	String s = sc.nextLine();
+	if(s.equals("OCT 31") || s.equals("DEC 25")){
+	    System.out.println("yup");
+	} else {
+	    System.out.println("nope");
+	}
 	
     }
 }
