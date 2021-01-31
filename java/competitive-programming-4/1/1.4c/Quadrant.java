7b7c301050a4be5f1cb198a734d07b75d4b6512c
John Patrick Amata <johnamata97@gmail.com>
2021-01-31 20:14:10 +0800

    cp4 grind

---
+++ java/competitive-programming-4/1/1.4c/Quadrant.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Quadrant{
+    public static void main(String[]args){
+	Scanner sc = new Scanner(System.in);
+	int x = sc.nextInt();
+	int y = sc.nextInt();
+	if(x >= 0 && y>= 0 ){
+	    System.out.println("1");
+	}
+	else if(x < 0 && y>= 1) System.out.println("2");
+	else if(x < 0 && y < 0) System.out.println("3");
+	else if(x >= 0 && y < 0) System.out.println("4");
+    }
+}
