ca872666240735ddfe11c12c2f22d4d1e320758b
John Patrick Amata <johnamata97@gmail.com>
2021-06-29 19:57:34 +0800

    keep solving

--- cpp/competitive-programming-4/ch01/Planina.cpp
+++ cpp/competitive-programming-4/ch01/Planina.cpp
@@ -5,7 +5,14 @@ Hint: just print (2^N+1)^2; OEIS A028400
 */
 
 #include <iostream>
+#include <iomanip>
+#include <math.h>
 using namespace std;
 int main(){
- return 0;   
+    int n;
+    cin >> n;
+    //set decimal precision to pass test cases in 8 and above
+    //alternatively we could probably cast it to int, but i used setprecision here to learn more about C++ libraries
+    cout << fixed << setprecision(0) << pow(pow(2, n)+1, 2);
+    return 0;   
 }
