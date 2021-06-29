f426d3f58b6d26f23a2227d456043024545be460
John Patrick Amata <johnamata97@gmail.com>
2021-06-29 19:35:31 +0800

    keep solving

--- cpp/competitive-programming-4/ch01/JackOLanternJuxtaposition.cpp
+++ cpp/competitive-programming-4/ch01/JackOLanternJuxtaposition.cpp
@@ -6,5 +6,12 @@ Hint: just print N*T*M
 #include <iostream>
 using namespace std;
 int main(){
- return 0;   
+  int n, t, m;
+  //n = eye designs
+  //t = nose designs
+  //m = mouth designs
+  cin >> n >> t >> m;
+  //outputs all probable cases
+  cout << (n * t * m);
+  return 0;
 }
