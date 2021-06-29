a6b3b3d164bdbe1c9e4ff6330b3349630be2af82
John Patrick Amata <johnamata97@gmail.com>
2021-06-29 16:02:24 +0800

    keep solving

--- cpp/competitive-programming-4/ch01/Planina.cpp
+++ cpp/competitive-programming-4/ch01/Planina.cpp
@@ -1,3 +1,9 @@
+/* 
+https://open.kattis.com/problems/planina
+Category: 1.4a, I/O + Sequences Only
+Hint: just print (2^N+1)^2; OEIS A028400 
+*/
+
 #include <iostream>
 using namespace std;
 int main(){
--- cpp/competitive-programming-4/ch01/R2.cpp
+++ cpp/competitive-programming-4/ch01/R2.cpp
@@ -1,8 +1,4 @@
-/* 
-https://open.kattis.com/problems/planina
-Category: 1.4a, I/O + Sequences Only
-Hint: just print (2^N+1)^2; OEIS A028400 
-*/
+
 
 #include <iostream>
 using namespace std;
