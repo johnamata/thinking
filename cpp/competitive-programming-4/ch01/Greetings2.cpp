43ac8a15d45ee957c3a4614e34de96edfd571c06
John Patrick Amata <johnamata97@gmail.com>
2021-06-29 16:52:47 +0800

    keep solving

--- cpp/competitive-programming-4/ch01/Greetings2.cpp
+++ cpp/competitive-programming-4/ch01/Greetings2.cpp
@@ -6,5 +6,14 @@ Hint: just reprint the input as requested
 #include <iostream>
 using namespace std;
 int main(){
- return 0;   
+    string s;
+    int count;
+    cin >> s;
+    cout << "h";
+    //there's probably a C++ inbuilt function the solution cleaner
+    //but as of now, this is the current state of my C++ knowledge lmao, i actually just learned about .length() thanks to this question
+    for(int i = 1; i < s.length() - 1; i++) cout << "e";
+    for(int i = 1; i < s.length() - 1; i++) cout << "e";
+    cout << "y";
+    return 0;   
 }
