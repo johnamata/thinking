4b0db1028bdf8c87953d89f8147d257326a426e0
John Patrick Amata <johnamata97@gmail.com>
2021-06-29 19:33:18 +0800

    keep solving

--- cpp/competitive-programming-4/ch01/R2.cpp
+++ cpp/competitive-programming-4/ch01/R2.cpp
@@ -7,5 +7,11 @@ Hint: just print 2*S-R1
 #include <iostream>
 using namespace std;
 int main(){
- return 0;   
+    //mean is gotten by
+    //(R1+R2)/2
+    //in this problem, we have R1 and mean, but no R2
+    int r1, mean;
+    cin >> r1 >> mean;
+    cout << 2 * mean - r1;
+    return 0;   
 }
