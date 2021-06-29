722ea9ce8469909662cbf695903fa7d009f29756
John Patrick Amata <johnamata97@gmail.com>
2021-06-29 20:11:47 +0800

    keep solving

---
+++ cpp/competitive-programming-4/ch01/1.4 a IO-Sequences/Romans.cpp
@@ -0,0 +1,13 @@
+/*
+https://open.kattis.com/problems/romans
+Category: 1.4a, I/O + Sequences Only
+Hint: just print round(X * 1087.7626)
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
---
+++ cpp/competitive-programming-4/ch01/1.4 b Repetition Only/Different.cpp
@@ -0,0 +1,13 @@
+/*
+https://open.kattis.com/problems/different
+Category: 1.4b, Repetition Only
+Hint: use abs function per test case
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
