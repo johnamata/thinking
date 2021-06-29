43fb173d73b0beb21747a5690ed0decb59efe1b8
John Patrick Amata <johnamata97@gmail.com>
2021-06-29 16:05:54 +0800

    keep solving

--- cpp/competitive-programming-4/ch01/Hello.cpp
+++ cpp/competitive-programming-4/ch01/Hello.cpp
@@ -1,5 +1,14 @@
+/*
+https://open.kattis.com/problems/hello
+Category: 1.4a, I/O + Sequences Only
+Hint: just print "Hello World!"
+*/
+
 #include <iostream>
 using namespace std;
 int main(){
- return 0;   
+    cout << "Hello World!";
+    //the kattis judge wasnt case sensitive though, as this also worked:
+    //cout << "HELLO world!";
+    return 0;   
 }
