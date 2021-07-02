86d7af61f6bbb7145b396b960a338669ada50c33
John Patrick Amata <johnamata97@gmail.com>
2021-07-02 20:57:32 +0800

    keep solving

--- cpp/competitive-programming-4/ch01/1.4 b Repetition Only/Qaly.cpp
+++ cpp/competitive-programming-4/ch01/1.4 b Repetition Only/Qaly.cpp
@@ -9,5 +9,15 @@ Hint: trivial loop
 using namespace std;
 
 int main(){
+    int n;
+    double q1, q2, qaly;
+    cin >> n;
+    int i;
+    while (i < n){
+        cin >> q1 >> q2;
+        qaly += q1 * q2;
+        i++;
+    }
+    cout << qaly;
     return 0;
 }
