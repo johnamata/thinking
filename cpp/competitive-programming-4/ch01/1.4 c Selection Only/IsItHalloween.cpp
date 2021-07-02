6d0664058701cf6d0be4fee280a34d9ba06c4dfe
John Patrick Amata <johnamata97@gmail.com>
2021-07-02 21:47:43 +0800

    keep solving

---
+++ cpp/competitive-programming-4/ch01/1.4 c Selection Only/IsItHalloween.cpp
@@ -0,0 +1,13 @@
+/*
+https://open.kattis.com/problems/isithalloween
+Category: 1.4c, Selection Only
+Hint: if-else; 2 cases
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
