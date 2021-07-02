cbab8c0e35e48c00eba0288352981d60dede1021
John Patrick Amata <johnamata97@gmail.com>
2021-07-02 20:51:10 +0800

    keep solving

--- cpp/competitive-programming-4/ch01/1.4 b Repetition Only/Tarifa.cpp
+++ cpp/competitive-programming-4/ch01/1.4 b Repetition Only/Tarifa.cpp
@@ -1,7 +1,7 @@
 /*
-https://open.kattis.com/problems/tarifa
+https://open.kattis.com/problems/qaly
 Category: 1.4b, Repetition Only
-Hint: one pass; array not needed
+Hint: trivial loop
 */
 
 #include <iostream>
@@ -9,5 +9,21 @@ Hint: one pass; array not needed
 using namespace std;
 
 int main(){
+    /*
+    thinking, in case i forget the steps
+    - sum the total megs first plus extra month
+    - sum the total usage first
+    - subtract the total megs and total usage
+    */
+    int plan, months, ans, usage, total_usage = 0, i = 0;
+    cin >> plan >> months;
+    int total_megs = plan * (months + 1);
+    while(i < months){
+        cin >> usage;
+        total_usage += usage;
+        
+        i++;
+    }
+    cout << total_megs - total_usage ;
     return 0;
 }
