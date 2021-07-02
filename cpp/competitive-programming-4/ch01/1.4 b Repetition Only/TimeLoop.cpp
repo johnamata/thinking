e5d7623bec6e53c07eddb85c87a9841cdd26c0a2
John Patrick Amata <johnamata97@gmail.com>
2021-07-02 20:12:05 +0800

    keep solving

--- cpp/competitive-programming-4/ch01/1.4 b Repetition Only/TimeLoop.cpp
+++ cpp/competitive-programming-4/ch01/1.4 b Repetition Only/TimeLoop.cpp
@@ -9,5 +9,21 @@ Hint: just print 'num Abracadabra' N times
 using namespace std;
 
 int main(){
+    int n, i =1;
+    cin >> n;
+    while(i <= n){
+        /*
+        i tried to do 
+        cout << i + " Abracadabra" << endl;
+        but it gave me the following error
+
+        warning: adding 'int' to a string does not append to the
+      string [-Wstring-plus-int]
+        cout << i + " Abracadabra"
+        */
+        cout << i << " Abracadabra" << endl;
+        i++;
+    }
+
     return 0;
 }
