52b61c26edb7e6a3913b4e522cab304e9f51da93
John Patrick Amata <johnamata97@gmail.com>
2021-07-02 20:03:20 +0800

    keep solving

---
+++ cpp/competitive-programming-4/ch01/1.4 b Repetition Only/TimeLoop.cpp
@@ -0,0 +1,13 @@
+/*
+https://open.kattis.com/problems/timeloop
+Category: 1.4b, Repetition Only
+Hint: just print 'num Abracadabra' N times
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
