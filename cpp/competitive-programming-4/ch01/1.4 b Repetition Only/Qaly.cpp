eb355d69db3408527e3d99506ca9691d45a91e5b
John Patrick Amata <johnamata97@gmail.com>
2021-07-02 20:05:00 +0800

    keep solving

--- cpp/competitive-programming-4/ch01/1.4 b Repetition Only/Qaly.cpp
+++ cpp/competitive-programming-4/ch01/1.4 b Repetition Only/Qaly.cpp
@@ -1,3 +1,9 @@
+/*
+https://open.kattis.com/problems/qaly
+Category: 1.4b, Repetition Only
+Hint: trivial loop
+*/
+
 #include <iostream>
 
 using namespace std;
--- cpp/competitive-programming-4/ch01/1.4 b Repetition Only/Tarifa.cpp
+++ cpp/competitive-programming-4/ch01/1.4 b Repetition Only/Tarifa.cpp
@@ -1,3 +1,9 @@
+/*
+https://open.kattis.com/problems/tarifa
+Category: 1.4b, Repetition Only
+Hint: one pass; array not needed
+*/
+
 #include <iostream>
 
 using namespace std;
